fb9e6f90cd06d6039c7a85f984c8ddf1544114fc
SebfailedAgain <aspiring.programmer69@gmail.com>
2025-11-18 11:14:03 -0500

    Implemented Logs constructor, which will create the specified log file if it doesn't exist

--- bank/Logs.java
+++ bank/Logs.java
@@ -2,10 +2,27 @@ package bank;
 
 public class Logs {
     private final String textFile;
-
+    
     public Logs(String textFile) {
         this.textFile = textFile;
-        // Future: initialize file handlers, ensure file exists, etc.
+        createLogFile();        
+    }
+    
+    private void createLogFile() {
+    	try {
+    		File logFile = new File(textFile);
+    		
+			if (logFile.createNewFile()) {
+				System.out.println(String.format("Successfully created log file with name %s.", this.textFile));
+			}
+			else {
+				System.out.println(String.format("Log file with name %s already exists.", this.textFile));
+			}
+		} 
+    	catch (IOException e) {
+			System.out.println(String.format("An error occured while trying to create LOG text file with name %s.", this.textFile));
+			e.printStackTrace();
+		}
     }
 
     public void append(String actor, String action, String target, String details) {
