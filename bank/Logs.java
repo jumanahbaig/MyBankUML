70370f8b70f6766641f0dcd869860e99728ed217
SebfailedAgain <aspiring.programmer69@gmail.com>
2025-11-18 11:36:33 -0500

    Implemented ListAll using BufferedReader

--- bank/Logs.java
+++ bank/Logs.java
@@ -54,8 +54,21 @@ public class Logs {
     	return now.format(formatter);
     }
 
-    public void listAll() {
-        // TODO: read the entire log file and print every entry to the console.
+    public void listAll() {    	
+    	try (BufferedReader reader = new BufferedReader(new FileReader(this.textFile))) {
+    		String line;
+    		while((line = reader.readLine()) != null) {
+    			System.out.println(line);
+    		}
+    	} 
+    	catch (FileNotFoundException e) {
+    		System.out.println(String.format("Could not find LOG text file with name %s when attempting to list all of its entries.", this.textFile));
+			e.printStackTrace();
+		} 
+    	catch (IOException e) {
+    		System.out.println(String.format("An error occured while trying to list all entries of LOG text file with name %s.", this.textFile));
+			e.printStackTrace();
+		}
     }
 
     public void listByUser(String actor) {
