f64b1cbfa510e18afa4b84a3497f1dae11763587
UndeadBat0197 <pmaximed@videotron.ca>
2025-11-21 14:49:34 -0500

    finish this section, let me know if you want  somehting changed

--- bank/Transaction.java
+++ bank/Transaction.java
@@ -1,13 +1,45 @@
 package bank;
+import lombok.Getter;
 
+@Getter
 public class Transaction {
+
+    //to identify transaction ex 100001, 100002, 200001 etc
+    int id;
+    //to track amount $ involved in transaction
+    double amount;
+    //to track what the transaction is about ex: "Deposit", "Withdrawal for rent" etc
+    String receipt;
+    //to track whether transaction is paid or not, if so add to account balance, if not do nothing
+    bool paid;
+    //use to track whether transaction is deposit or withdrawal(aka increse or decrease in account balance)
+    bool is_deposit;
+
+    Transaction(int id, double amount,bool is_deposit, String receipt) {
+        //id will be given when transaction is created by the account, 
+        //so each account can have same id for their respectif transactions
+        this.id = id;
+        this.amount = amount;
+        //true for deposit, false for withdrawal
+        this.is_deposit = is_deposit;
+        //generate receipt based on type of transaction
+        if(is_deposit){
+            this.paid = true; //deposits are always "paid", added to balance immediately
+            this.receipt = "Deposit of amount: " + amount;
+        } else {
+            this.paid = false; //withdrawals need to be paid, i.e., processed
+            this.receipt = "Withdrawal for: " + receipt + " of amount: " + amount;
+        }
+    }
+
+    
     public void pay() {
-        // TODO: track amount, currency, and timestamps for every transaction.
+        this.paid = true;
         System.out.println("Payment transaction is done.");
     }
 
-    public void receipt() {
-        // TODO: wire this up to customer notifications (email/SMS) and audit logs.
-        System.out.println("Transaction receipt.");
-    }
+    //this as been replace by info field getter
+    // public void receipt() {
+    //     System.out.println("Transaction receipt.");
+    // }
 }
